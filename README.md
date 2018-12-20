05492e87b9db86dec043a8c7bee9d516b8c5736f
taching <taching@aliyun.com>
2018-12-20 10:49:08 +0800

    up

--- README.md
+++ README.md
@@ -5,9 +5,26 @@ export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
 export PATH=/Users/hello/flutter/bin:$PATH
 #export PATH=PATH_TO_FLUTTER_GIT_DIRECTORY/flutter/bin:$PATH
 
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/emulator
+
+
+export PATH=/Users/用户名/Documents/flutter/flutter/bin:$PATH
+export ANDROID_HOME="/Users/用户名/Documents/android_sdk" //android sdk目录，替换为你自己的即可
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+
 ```
 ```
 运行 flutter doctor
 运行以下命令查看是否需要安装其它依赖项来完成安装：
 flutter doctor
+```
+```
+flutter run
 ```
--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -78,7 +78,7 @@ class MyApp extends StatelessWidget {
     Widget textSection = Container(
       padding: const EdgeInsets.all(32.0),
       child: Text(
-        '''Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        '''Lak1111e Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
         ''',
         softWrap: true,
       ),
