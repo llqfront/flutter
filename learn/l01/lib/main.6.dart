ce75471b3300c05cf7e4a6691c5c53507b853259
taching <taching@aliyun.com>
2018-12-26 16:12:48 +0800

    up

---
+++ learn/l01/lib/main.6.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First Flutter'),
+        ),
+        body: Center(
+          child: Container(
+            height: 200.0,
+            child: MyList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+          width: 180.0,
+          color: Colors.lightBlue,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.lightGreen,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.lightBlue,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.lightGreen,
+        ),
+      ],
+    );
+  }
+}
