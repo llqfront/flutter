bf0edb306a6db084fa232716432df96210cd4a8a
taching <taching@aliyun.com>
2018-12-26 15:24:29 +0800

    up

--- learn/l01/lib/main.dart
+++ learn/l01/lib/main.dart
@@ -6,33 +6,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'fisrt Flutter',
+      title: 'First Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('fisrt Flutter'),
+          title: Text('First Flutter'),
         ),
-        body: Center(
-          child: Container(
-            child: new Image.network(
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+              leading: new Icon(Icons.access_time),
+              title: new Text('access_time'),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.access_time),
+              title: new Text('access_time'),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.access_time),
+              title: new Text('access_time'),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.access_time),
+              title: new Text('access_time'),
+            ),
+            new Image.network(
               'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=330275530,1504313412&fm=27&gp=0.jpg',
-              // scale: 6.0,
-              fit: BoxFit.contain,
-              color: Colors.greenAccent,
-              colorBlendMode: BlendMode.darken,
-              repeat: ImageRepeat.repeat,
+              width: 100.0,
+              height: 100.0,
             ),
-            width: 300.0,
-            height: 300.0,
-            // color: Colors.lightBlue,
-            alignment: Alignment.centerLeft,
-            decoration: new BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple],
-              ),
-              border: Border.all(width: 2.0, color: Colors.red),
-            ),
-            // margin: const EdgeInsets.all(10.0),
-          ),
+          ],
         ),
       ),
     );
