bafb6572b11a6326b32ef2ab21f85eaa24539032
taching <taching@aliyun.com>
2018-12-26 15:28:08 +0800

    up

--- learn/l01/lib/main.dart
+++ learn/l01/lib/main.dart
@@ -11,30 +11,32 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('First Flutter'),
         ),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-              leading: new Icon(Icons.access_time),
-              title: new Text('access_time'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.access_time),
-              title: new Text('access_time'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.access_time),
-              title: new Text('access_time'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.access_time),
-              title: new Text('access_time'),
-            ),
-            new Image.network(
-              'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=330275530,1504313412&fm=27&gp=0.jpg',
-              width: 100.0,
-              height: 100.0,
-            ),
-          ],
+        body: Container(
+          child: new ListView(
+            children: <Widget>[
+              new ListTile(
+                leading: new Icon(Icons.access_time),
+                title: new Text('access_time'),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.access_time),
+                title: new Text('access_time'),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.access_time),
+                title: new Text('access_time'),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.access_time),
+                title: new Text('access_time'),
+              ),
+              new Image.network(
+                'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=330275530,1504313412&fm=27&gp=0.jpg',
+                width: 100.0,
+                height: 100.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
