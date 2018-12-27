8e4d2d3cec6859c8fcf0d90808e5860ce31483f5
taching <taching@aliyun.com>
2018-12-27 19:06:40 +0800

    up

--- flutter_ydfast/lib/main.dart
+++ flutter_ydfast/lib/main.dart
@@ -98,45 +98,103 @@ class MyApp extends StatelessWidget {
               ),
             ),
             new Container(
-                height: 44.0,
-                color: Colors.white,
-                margin: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
-                child: new Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: new Container(
-                        child: new Text(
-                          '交易量',
-                          textAlign: TextAlign.center,
-                        ),
+              height: 44.0,
+              color: Colors.white,
+              margin: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+              child: new Row(
+                children: <Widget>[
+                  Expanded(
+                    child: new Container(
+                      child: new Text(
+                        '交易量',
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                    Expanded(
-                      child: new Container(
-                        child: new Text(
-                          '人气',
-                          textAlign: TextAlign.center,
-                        ),
+                  ),
+                  Expanded(
+                    child: new Container(
+                      child: new Text(
+                        '人气',
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                    Expanded(
-                      child: new Container(
-                        child: new Text(
-                          '活跃',
-                          textAlign: TextAlign.center,
-                        ),
+                  ),
+                  Expanded(
+                    child: new Container(
+                      child: new Text(
+                        '活跃',
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                    Expanded(
-                      child: new Container(
-                        child: new Text(
-                          '距离',
-                          textAlign: TextAlign.center,
-                        ),
+                  ),
+                  Expanded(
+                    child: new Container(
+                      child: new Text(
+                        '距离',
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                  ],
-                ))
+                  ),
+                ],
+              ),
+            ),
+            new Container(
+              height: 1000.0,
+              child: GridView(
+                // body:GridView.count(
+                padding: const EdgeInsets.all(20.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisSpacing: 10.0,
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 2.0,
+                  childAspectRatio: 0.7,
+                ),
+                children: <Widget>[
+                  const Text('adsfasdf'),
+                  const Text('adsfasdf'),
+                  const Text('adsfasdf'),
+                  const Text('adsfasdf'),
+                  const Text('adsfasdf'),
+                  const Text('adsfasdf'),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  new Image.network(
+                    'http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
