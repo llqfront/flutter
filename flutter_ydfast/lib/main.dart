76444d7c2e0028d045a6f3aa86b187e1b6c977b8
taching <taching@aliyun.com>
2018-12-29 15:41:41 +0800

    up

--- flutter_ydfast/lib/main.dart
+++ flutter_ydfast/lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:async' show Future, Timer;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ydfast/swiper_list.dart';
-import 'dart:async' show Future, Timer;
 import 'package:flutter_ydfast/Refresh.dart';
 import 'package:english_words/english_words.dart';
 
@@ -226,51 +227,51 @@ class _MyHomePageState extends State<MyHomePage> {
                       ],
                     ),
                   ),
-                  new Container(
-                    height: 599.0,
-                    child: new CustomScrollView(
-                      slivers: <Widget>[
-                        const SliverAppBar(
-                          pinned: true,
-                          expandedHeight: 250.0,
-                          flexibleSpace: FlexibleSpaceBar(
-                            title: Text('Demo'),
-                          ),
-                        ),
-                        SliverGrid(
-                          gridDelegate:
-                              SliverGridDelegateWithMaxCrossAxisExtent(
-                            maxCrossAxisExtent: 200.0,
-                            mainAxisSpacing: 10.0,
-                            crossAxisSpacing: 10.0,
-                            childAspectRatio: 4.0,
-                          ),
-                          delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                              return Container(
-                                alignment: Alignment.center,
-                                color: Colors.teal[100 * (index % 9)],
-                                child: Text('grid item $index'),
-                              );
-                            },
-                            childCount: 20,
-                          ),
-                        ),
-                        SliverFixedExtentList(
-                          itemExtent: 50.0,
-                          delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                              return Container(
-                                alignment: Alignment.center,
-                                color: Colors.lightBlue[100 * (index % 9)],
-                                child: Text('list item $index'),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  // new Container(
+                  //   height: 599.0,
+                  //   child: new CustomScrollView(
+                  //     slivers: <Widget>[
+                  //       const SliverAppBar(
+                  //         pinned: true,
+                  //         expandedHeight: 250.0,
+                  //         flexibleSpace: FlexibleSpaceBar(
+                  //           title: Text('Demo'),
+                  //         ),
+                  //       ),
+                  //       SliverGrid(
+                  //         gridDelegate:
+                  //             SliverGridDelegateWithMaxCrossAxisExtent(
+                  //           maxCrossAxisExtent: 200.0,
+                  //           mainAxisSpacing: 10.0,
+                  //           crossAxisSpacing: 10.0,
+                  //           childAspectRatio: 4.0,
+                  //         ),
+                  //         delegate: SliverChildBuilderDelegate(
+                  //           (BuildContext context, int index) {
+                  //             return Container(
+                  //               alignment: Alignment.center,
+                  //               color: Colors.teal[100 * (index % 9)],
+                  //               child: Text('grid item $index'),
+                  //             );
+                  //           },
+                  //           childCount: 20,
+                  //         ),
+                  //       ),
+                  //       SliverFixedExtentList(
+                  //         itemExtent: 50.0,
+                  //         delegate: SliverChildBuilderDelegate(
+                  //           (BuildContext context, int index) {
+                  //             return Container(
+                  //               alignment: Alignment.center,
+                  //               color: Colors.lightBlue[100 * (index % 9)],
+                  //               child: Text('list item $index'),
+                  //             );
+                  //           },
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
                 ],
               ),
               loadingBuilder: (BuildContext context) {
